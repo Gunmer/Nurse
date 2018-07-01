fd14005f7443c47a6b9c79ec9f252c906a2c34b0
Gunmer <csosaur@gmail.com>
2018-07-02 00:59:57 +0200

    Update version

--- Example/Nurse/ViewController.swift
+++ Example/Nurse/ViewController.swift
@@ -6,24 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        do {
-//            Nurse.add(module: AppModule.self)
-//
-//            let renault = try Nurse.injector.getInstance(of: Car.self)
-//            let ford = try Nurse.injector.getInstance(of: Car.self, name: "Ford")
-//
-//            assert(renault is Renault)
-//            assert(ford is Ford)
-//            assert(!renault.wheels.isEmpty)
-//        } catch {
-//            print(error)
-//            assertionFailure(error.localizedDescription)
-//        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
